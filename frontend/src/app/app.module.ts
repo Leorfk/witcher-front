cff03937937be98c0bdc964cdf1ed46aeb4e35bc
leonardo <leonardorodrigues_f@hotmail.com>
2020-07-18 11:53:44 -0300

    Criacao do header da aplicacao

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './componets/template/header/header.component';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+const MaterialModules = [
+  MatToolbarModule
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent
+  ],
+  imports: [
+    MaterialModules,
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
