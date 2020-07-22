8b8896efd1d5d0b2763c242eeca9a0a3fbc1907f
leonardo <leonardorodrigues_f@hotmail.com>
2020-07-21 21:45:58 -0300

    Criacao da logica de delete

--- frontend/src/app/componets/character/character-delete/character-delete.component.ts
+++ frontend/src/app/componets/character/character-delete/character-delete.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CharacterService } from '../character-service/character.service';
+import { Character } from '../character.model';
 
 @Component({
   selector: 'app-character-delete',
@@ -7,9 +10,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CharacterDeleteComponent implements OnInit {
 
-  constructor() { }
+  character: Character = {
+    name: '',
+    age: 0,
+    abilities: [],
+    affiliation: [],
+    aliases: '',
+    eyeColor: '',
+    hairColor: '',
+    gender: '',
+    profession: '',
+    skin: ''
+  };
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private characterService: CharacterService) { }
 
   ngOnInit(): void {
+    this.characterService.getById(this.route.snapshot.paramMap.get('id')).subscribe(x => {
+      this.character = x;
+    });
   }
 
+  delete(): void {
+    this.characterService.delete(this.character).subscribe(() => {
+      this.characterService.showMessage(`Personagem ${this.character.name}, foi excluido!!!`);
+      this.router.navigate(['/characters']);
+    });
+  }
+
+  cancel(): void {
+    this.router.navigate(['/characters']);
+  }
 }
