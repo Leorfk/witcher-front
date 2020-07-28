a277d5c59a7d87f9154148502d0a311ca819d2e8
Leonardo <leonardorodrigues_f@hotmail.com>
2020-07-27 23:58:40 -0300

    Teste de cliks na tabela

--- frontend/src/app/componets/util/table/table.component.ts
+++ frontend/src/app/componets/util/table/table.component.ts
@@ -1,3 +1,5 @@
+import { CharacterService } from './../../character/character-service/character.service';
+import { Character } from './../../character/character.model';
 import { TableConfig } from './table-config.model';
 import { Component, Input, OnInit } from '@angular/core';
 
@@ -13,8 +15,18 @@ export class TableComponent implements OnInit {
   displayedColumns: TableConfig;
   @Input()
   routes: any[];
+  character: Character[] = [];
 
-  constructor() {}
+  constructor(private characterService: CharacterService) {}
+
+  toma(char: Character): void{
+    console.log(char);
+    char.id = null;
+    this.characterService.create(char).subscribe(x => {
+      this.characterService.showMessage(`The ${x.name} has been add to game`);
+    });
+    this.characterService.getAll().subscribe(x => { this.dataSource = x});
+  }
 
   ngOnInit(): void {
   }
